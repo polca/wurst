edf732a367e6060dc10b11e2e7689b9f36ad9e79
Chris Mutel <cmutel@gmail.com>
2017-01-19 17:15:32 +0100

    Initial checkin with IMAGE importer

---
+++ wurst/IMAGE/__init__.py
@@ -0,0 +1,7 @@
+# Change emissions based on percentage change in IMAGE results (trust ecoinvent absolute numbers)
+# Change relative shares of technology in mixes
+# Change energy efficiency of exchanges
+
+
+from .metadata import REGIONS
+from .io import load_image_data_file
