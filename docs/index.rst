9285e6d4cb072ac95aeab3c030be7a6eb802ef69
Chris Mutel <cmutel@gmail.com>
2017-06-21 13:31:09 +0200

    Add initial project structure

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. wurst documentation master file, created by
+   sphinx-quickstart on Wed Jun 21 13:30:42 2017.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to wurst's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup
+import os
+
+packages = []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk('wurst'):
+    # Ignore dirnames that start with '.'
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+
+setup(
+    name='wurst',
+    version="0.1.dev",
+    packages=packages,
+    author="Chris Mutel",
+    author_email="cmutel@gmail.com",
+    license=open('LICENSE').read(),
+    install_requires=[
+        "brightway2",
+        "numpy",
+        "pandas",
+    ],
+    url="https://github.com/cmutel/wurst",
+    long_description=open('README.md').read(),
+    description=('Wurst is a python package for linking and modifying '
+                 'industrial ecology models'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+)
